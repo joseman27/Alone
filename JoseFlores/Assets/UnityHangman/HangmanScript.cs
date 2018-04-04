37e1edd3340e7895b7e597038ed5db800357a8a8
joseman27 <josemanuelfm27@gmail.com>
2018-04-03 22:06:43 -0500

    agregue todo

---
+++ JoseFlores/Assets/UnityHangman/HangmanScript.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HangmanScript : MonoBehaviour {
+
+    public string palabra;
+    string palabraEscondida;
+
+    public Text outputText;
+    public InputField inputText;
+
+    public AudioClip successSound;
+    public AudioClip failSound;
+    public AudioSource camSource;
+
+    // Use this for initialization
+    void Start()
+    {
+        inputText.Select();
+        foreach (char c in palabra)
+        {
+            palabraEscondida += "*";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (outputText.text != palabraEscondida)
+        {
+            outputText.text = palabraEscondida;
+        }
+        if (Input.GetKeyDown(KeyCode.Return) && !string.IsNullOrEmpty(inputText.text))
+        {
+            string letra = inputText.text.Substring(0, 1);
+            if (palabra.Contains(letra))
+            {
+                string palabraTemporal = "";
+                for (int i = 0; i < palabra.Length; i++)
+                {
+                    if (palabra[i] == letra[0])
+                    {
+                        palabraTemporal += letra;
+                    }
+                    else
+                    {
+                        palabraTemporal += palabraEscondida[i];
+                    }
+                }
+                palabraEscondida = palabraTemporal;
+                camSource.PlayOneShot(successSound);
+            }
+            else
+            {
+                camSource.PlayOneShot(failSound);
+            }
+            inputText.text = "";
+            inputText.Select();
+            inputText.ActivateInputField();
+        }
+        if (palabraEscondida == palabra)
+        {
+            Debug.Log("FELICIDADES!");
+        }
+    }
+}
