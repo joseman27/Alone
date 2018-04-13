6935be74311598e301851b8ebd0eefec1b86a6a9
JoseManuelFlores <josemanuelfm27@gmail.com>
2018-04-12 22:07:24 -0500

    asdfgh

---
+++ JoseFlores/Assets/BlueAreaScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueAreaScript : MonoBehaviour { 
+
+      public Light targetLight;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag ("Player")) {
+         targetLight.enabled= true;
+        }
+    }
+}
--- JoseFlores/Assets/Scripts/ControldelJugador.cs
+++ JoseFlores/Assets/Scripts/ControldelJugador.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class ControldelJugador : MonoBehaviour {
     public float speed;
     Vector3 InicialPosition;
+    Rigidbody playerRb;
+
+
 
     // Use this for initialization
     void Start()
     {
+        playerRb = GetComponent<Rigidbody>();
         InicialPosition = transform.position; 
     }
     // Update is called once per frame
@@ -17,20 +21,24 @@ public class ControldelJugador : MonoBehaviour {
 
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(new Vector3(-35, 0,0) * Time.deltaTime);
+            //transform.Rotate(new Vector3(-35, 0,0) * Time.deltaTime);
+            playerRb.MovePosition(transform.position + (Vector3.left * speed * Time.deltaTime));
            
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(new Vector3(35,0,0) * Time.deltaTime);
+            //transform.Rotate(new Vector3(35,0,0) * Time.deltaTime);
+            playerRb.MovePosition(transform.position + (Vector3.right * speed * Time.deltaTime));
         }
         if (Input.GetKey(KeyCode.W))
         {
-            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            //transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            playerRb.MovePosition(transform.position + (Vector3.forward * speed * Time.deltaTime));
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.Translate(Vector3.back * speed * Time.deltaTime);
+            //transform.Translate(Vector3.back * speed * Time.deltaTime);
+            playerRb.MovePosition(transform.position + (Vector3.back * speed * Time.deltaTime));
         }
     }
    public void ResetPosition() {
